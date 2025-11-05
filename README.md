8f5bf67043fb1584421ef410ce6294da2fa0de65
Aruide <ruide.aurelien@gmail.com>
2025-11-05 09:19:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# audiomancy_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/audiomancy_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.audiomancy_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
